a6d6a1a17e02f660fb115c66390db08ee052ed67
sdahal-dev <sdahal.contact@gmail.com>
2025-07-29 16:06:13 -0500

    Initial commit - base weather site

---
+++ script.js
@@ -0,0 +1,102 @@
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+
+const apiKey = "0a6e6936f2dfe122841978d40b7cf9eb";
+
+weatherForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const city = cityInput.value;
+
+  if (city) {
+    try {
+      const weatherData = await getWeather(city);
+      displayWeather(weatherData);
+    } catch (error) {
+      console.error(error);
+      displayError(error);
+    }
+  } else {
+    displayError("Please enter a valid city name");
+  }
+});
+
+async function getWeather(city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  const response = await fetch(apiUrl);
+
+  if (!response.ok) {
+    throw new Error("Could not fetch weather data");
+  }
+
+  return await response.json();
+}
+
+function displayWeather(data) {
+  const {
+    name: city,
+    main: { temp, humidity },
+    weather: [{ description, id }],
+  } = data;
+
+  card.textContent = "";
+  card.style.display = "flex";
+
+  const cityDisplay = document.createElement("h1");
+  const tempDisplay = document.createElement("p");
+  const humidityDisplay = document.createElement("p");
+  const descriptionDisplay = document.createElement("p");
+  const weatherEmoji = document.createElement("p");
+
+  cityDisplay.textContent = city;
+  tempDisplay.textContent = `${Math.floor(((temp - 273.15) * 9) / 5 + 32)}°F`;
+  humidityDisplay.textContent = `Humidity: ${humidity}%`;
+  descriptionDisplay.textContent =
+    description.charAt(0).toUpperCase() + description.slice(1);
+  weatherEmoji.textContent = getWeatherEmoji(id);
+
+  cityDisplay.classList.add("cityDisplay");
+  tempDisplay.classList.add("tempDisplay");
+  humidityDisplay.classList.add("humidityDisplay");
+  descriptionDisplay.classList.add("descripitionDisplay");
+  weatherEmoji.classList.add("weatherEmoji");
+
+  card.appendChild(cityDisplay);
+  card.appendChild(tempDisplay);
+  card.appendChild(humidityDisplay);
+  card.appendChild(descriptionDisplay);
+  card.appendChild(weatherEmoji);
+}
+
+function getWeatherEmoji(weatherId) {
+  switch (true) {
+    case weatherId >= 200 && weatherId < 300:
+      return "⛈️"; // Thunderstorm
+    case weatherId >= 300 && weatherId < 500:
+      return "🌧️"; // Drizzle
+    case weatherId >= 500 && weatherId < 600:
+      return "🌧️"; // Rain
+    case weatherId >= 600 && weatherId < 700:
+      return "❄️"; // Snow
+    case weatherId >= 700 && weatherId < 800:
+      return "🌫️"; // Atmosphere
+    case weatherId === 800:
+      return "☀️"; // Clear
+    case weatherId > 800:
+      return "☁️"; // Clouds
+    default:
+      return "❓"; // Unknown
+  }
+}
+
+function displayError(message) {
+  const errorDisplay = document.createElement("p");
+  errorDisplay.textContent = message;
+  errorDisplay.classList.add("errorDisplay");
+
+  card.textContent = "";
+  card.style.display = "flex";
+  card.appendChild(errorDisplay);
+}
